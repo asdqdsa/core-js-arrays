cd1ad2574a49bbb0f9ffe3bcbe88a9ea22f8b915
asdqdsa <asdqdsa@protonmail.com>
2024-12-25 23:35:18 +0400

    feat: add solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -264,8 +264,12 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  return n < 2
+    ? Array(size).fill(0)
+    : Array(size)
+        .fill([])
+        .map(() => createNDimensionalArray(n - 1, size));
 }
 
 /**
@@ -280,7 +284,7 @@ function createNDimensionalArray(/* n, size */) {
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
 function flattenArray(nestedArray) {
-  return nestedArray.flat(2);
+  return nestedArray.flatMap((val) => (Array.isArray(val) ? val.flat() : val));
 }
 
 /**
@@ -296,8 +300,8 @@ function flattenArray(nestedArray) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap((val) => childrenSelector(val));
 }
 
 /**
@@ -313,10 +317,11 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, next) => {
+    return acc + next.reduce((product, val) => product - val);
+  }, 0);
 }
-
 /**
  * Breaks an array into chunks of the specified size.
  *
